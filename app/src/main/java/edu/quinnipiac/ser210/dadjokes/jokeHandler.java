c5089889a4defa45d9bc76326c32d8b36590212c
chrisrocco <cjrocco@quinnipiac.edu>
2022-03-17 20:14:28 -0400

    Async

---
+++ app/src/main/java/edu/quinnipiac/ser210/dadjokes/jokeHandler.java
@@ -0,0 +1,13 @@
+package edu.quinnipiac.ser210.dadjokes;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class jokeHandler {
+
+
+    public String getRandomJoke(String randomJokeJsonStr) throws JSONException {
+        JSONObject randomJokeJSONObj = new JSONObject(randomJokeJsonStr);
+        return randomJokeJSONObj.getString("text");
+    }
+}
