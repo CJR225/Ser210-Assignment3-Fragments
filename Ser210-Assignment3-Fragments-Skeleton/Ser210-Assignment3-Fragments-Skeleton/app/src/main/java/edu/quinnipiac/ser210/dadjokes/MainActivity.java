10ffcd76f79a5f889bbdbf74a801c020f0c52a2a
Rion McLaren <89875257+Ramclaren@users.noreply.github.com>
2022-04-10 12:42:28 -0400

    Add files via upload

---
+++ Ser210-Assignment3-Fragments-Skeleton/Ser210-Assignment3-Fragments-Skeleton/app/src/main/java/edu/quinnipiac/ser210/dadjokes/MainActivity.java
@@ -0,0 +1,197 @@
+/**
+ * MainActivity - Houses the async class which connects to the api api,
+ * connects to handler and outputs the variables in a arraylist to be retrieved using intent
+ *
+ * @author Chris Rocco & Rion-Mark Mclaren
+ * @date 3/21/22
+ * I dont know if it counts but the background is an image
+ */
+
+
+package edu.quinnipiac.ser210.dadjokes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String LOG_TAG = MainActivity.class.getSimpleName();
+    jokeHandler jkHandler = new jokeHandler();
+    ImageView mainbkgrnd;
+
+    private ShareActionProvider shareActionProvider;
+    private String url1 = ("https://dad-jokes.p.rapidapi.com/random/jokes");
+
+    /*
+    Sets the view, when the activity is created
+    Grabs the toolbar ID and adds it to the activity
+    Grabs the ID for the background image
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mainbkgrnd = findViewById(R.id.mainBkgrnd);
+    }
+
+    /*
+    On click method for the New Joke button, brings the user to the Joke activity, as well as fetching a new joke
+     */
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, JokesActivity.class);
+
+        new FetchJoke().execute();
+    }
+
+    /*
+    Share option for the action bar
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to share this dad joke?");
+
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    /*
+    Implementation for sharing a joke
+    */
+    private void setShareActionIntent(String text) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+
+
+    }
+
+    /*
+    Implementation for background changing in the action bar
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.blue_background:
+                mainbkgrnd.setImageResource(R.drawable.navy_bkgrnd);
+                break;
+            case R.id.red_background:
+                mainbkgrnd.setImageResource(R.drawable.red_and_black_synthwave_phone_wallpaper);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    /*
+    Grabs a random Joke from the API and sends it to the Joke Activity
+     */
+    class FetchJoke extends AsyncTask<ArrayList<String>, Void, ArrayList<String>> {
+
+        @Override
+        protected ArrayList<String> doInBackground(ArrayList<String>... arrayLists) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            ArrayList<String> joke;
+
+            try {
+                URL url = new URL(url1);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("X-RapidAPI-Key", "72e550e293msh0b4974c37ccabdap1e4627jsnabebc1c35c3d");
+                urlConnection.connect();
+
+                InputStream in = urlConnection.getInputStream();
+                if (in == null)
+                    return null;
+
+                reader = new BufferedReader(new InputStreamReader(in));
+                joke = getStringFromBuffer(reader);
+
+
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Error" + e.getMessage());
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Error" + e.getMessage());
+                        return null;
+                    }
+                }
+            }
+            return joke;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            if (result != null)
+                Log.d(LOG_TAG, "Result is null");
+            Intent intent = new Intent(MainActivity.this, JokesActivity.class);
+            intent.putExtra("Joke", result.get(0));
+            intent.putExtra("Punchline", result.get(1));
+            startActivity(intent);
+        }
+    }
+
+    private ArrayList<String> getStringFromBuffer(BufferedReader bufferedReader) {
+        StringBuffer buffer = new StringBuffer();
+        String line;
+
+        if (bufferedReader != null) {
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+                    buffer.append(line + '\n');
+                }
+                bufferedReader.close();
+                return jkHandler.getRandomJoke(buffer.toString());
+            } catch (Exception e) {
+                Log.e("MainActivity", "Error" + e.getMessage());
+                return null;
+            } finally {
+
+            }
+        }
+        return null;
+    }
+}
